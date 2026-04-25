0f6931ee98b4f28030960c7b0bf94e1df4f9f9aa
TheErickYT10 <erickpatino777@gmail.com>
2026-04-25 15:00:49 -0500

    parte 2 ejercicio1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        SolicitanteSubsidio.mostrarReglasSubcidio();
+        System.out.println("Ingrese su nombre completo: ");
+        String nombre= sc.nextLine();
+        System.out.println("Ingrese su cedula: ");
+        String cedula = sc.nextLine();
+        System.out.println("Ingrese sus ingresos mensuales: ");
+        double ingresosMensuales = sc.nextDouble();
+        System.out.println("Ingrese la cantidad de vehículos registrados: ");
+        int cantidadVehiculos = sc.nextInt();
+        System.out.println("Vive en Ecuador(True/False): ");
+        boolean viveEnEcuador = sc.nextBoolean();
+        SolicitanteSubsidio solicitante1 = new SolicitanteSubsidio(nombre,cedula,ingresosMensuales,cantidadVehiculos,viveEnEcuador);
+
+
+        System.out.println(solicitante1.toString());
+        solicitante1.generarResultado();
+
+        double consumoBase = solicitante1.calcularConsumoMensual();
+        double consumoExtra = solicitante1.calcularConsumoMensual(200);
+
+        System.out.println("\nConsumo mensual (base): " + consumoBase + " galones");
+        System.out.println("Consumo mensual (con km extra): " + consumoExtra + " galones");
+
 
     }
 }
--- src/SolicitanteSubsidio.java
+++ src/SolicitanteSubsidio.java
@@ -77,4 +77,50 @@ public class SolicitanteSubsidio {
                 this.cantidadVehiculos <= 1 &&
                 this.viveEnEcuador);
     }
+
+    public void generarResultado() {
+        if (subsidioAprobado()) {
+            System.out.println("Subsidio APROBADO");
+        } else {
+            System.out.println("Subsidio RECHAZADO");
+
+            if (ingresosMensuales > 1200)
+                System.out.println("- Ingresos altos");
+
+            if (cantidadVehiculos > 1)
+                System.out.println("- Tiene más de un vehículo");
+
+            if (!viveEnEcuador)
+                System.out.println("- No reside en Ecuador");
+        }
+    }
+
+    public static void  mostrarReglasSubcidio(){
+        System.out.println("==== Reglas para ontener el subcidio ===");
+        System.out.println("1. Tener ingresos mensuales menores o iguales a $1,200.");
+        System.out.println("2. No poseer mas de un vehiculo registrado. ");
+        System.out.println("3. Tener residencia en Ecuador. ");
+        System.out.println("==========================================================\n");
+    }
+
+    double calcularConsumoMensual(){
+        double kilometros  = 1000;
+        return kilometros/40;
+    }
+
+    public double calcularConsumoMensual( double kmExtra){
+        double Kilometros  = 1000+kmExtra;
+        return Kilometros / 40;
+    }
+
+    @Override
+    public String toString() {
+        return "\n===== Datos del solicitante =====\n" +
+                "Nombre: " + nombreCompleto + "\n" +
+                "Cédula: " + cedula + "\n" +
+                "Ingresos mensuales: $" + ingresosMensuales + "\n" +
+                "Cantidad de vehículos: " + cantidadVehiculos + "\n" +
+                "Vive en Ecuador: " + viveEnEcuador;
+    }
+
 }
