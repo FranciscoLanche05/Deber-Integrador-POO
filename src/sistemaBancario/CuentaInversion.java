17957776fda1923c344d8c0e06333347a10e2a4a
FranciscoLanche <paullanche4@gmail.com>
2026-04-25 23:00:04 -0500

    Parte 2 ejercicio 2

--- src/sistemaBancario/CuentaInversion.java
+++ src/sistemaBancario/CuentaInversion.java
@@ -1,8 +1,24 @@
 package sistemaBancario;
 
-public class CuentaInversion extends CuentaBancaria{
+public class CuentaInversion extends CuentaBancaria {
     public CuentaInversion(String numeroCuenta, String titular, double saldo) {
         super(numeroCuenta, titular, saldo);
+    }
+
+    @Override
+    public double calcularInteresMensual() {
+        double tasaAnual;
+        if (getSaldo() < 1000) tasaAnual = 0.04;
+        else if (getSaldo() <= 5000) tasaAnual = 0.05;
+        else tasaAnual = 0.06;
+        return (getSaldo() * tasaAnual) / 12;
+    }
+
+    @Override
+    public void retirar(double monto) {
+        if (monto > 0 && monto <= getSaldo()) {
+            setSaldo(getSaldo() - monto);
+        }
     }
 
 }
--- src/sistemaBancario/Main.java
+++ src/sistemaBancario/Main.java
@@ -1,7 +1,30 @@
 package sistemaBancario;
 
-public class Main {
-    public static void main(){
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List<CuentaBancaria> cuentas = new ArrayList<>();
+
+
+        cuentas.add(new CuentaAhorros("SAV-001", "Ana Pérez", 1200));
+        cuentas.add(new CuentaCorriente("CUR-002", "Luis Gómez", 800));
+        cuentas.add(new CuentaInversion("INV-003", "María López", 7000));
+
+        double totalInteresesBanco = 0;
+
+        System.out.println("===== REPORTE MENSUAL DEL BANCO =====");
+        for (CuentaBancaria c : cuentas) {
+            double interes = c.calcularInteresMensual();
+            totalInteresesBanco += interes;
+
+            System.out.println("Titular: " + c.getTitular());
+            System.out.println("Interés generado: $" + String.format("%.2f", interes));
+            System.out.println("Saldo actualizado: $" + String.format("%.2f", (c.getSaldo() + interes)));
+            System.out.println("-------------------------------------");
+        }
+
+        System.out.println("TOTAL INTERESES A PAGAR POR EL BANCO: $" + String.format("%.2f", totalInteresesBanco));
     }
 }
